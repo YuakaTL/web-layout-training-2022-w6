f306afae50eddedef8d81a0516152850f5d2ef92
YuakaTL <B10823036@yuntech.edu.tw>
2022-09-05 06:07:22 +0800

    Update 2022-09-04T22:07:17.404Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,16 +1,19 @@
+"use strict";
+
 // nav menu
-let menuBtn = document.querySelector('#menuBtn');
-let menuList = document.querySelector('#menuList');
+var menuBtn = document.querySelector('#menuBtn');
+var menuList = document.querySelector('#menuList');
 menuBtn.addEventListener("click", showMenu);
+
 function showMenu() {
   menuList.classList.toggle('max-h-0');
   menuList.classList.toggle('max-h-full');
-}
+} // 登入 Modal
 
-// 登入 Modal
-let loginDialog = document.querySelector('#loginDialog');
-let loginBtn = document.querySelector('#loginBtn');
-let closeLoginDialogBtn = document.querySelector("#closeLoginDialogBtn");
+
+var loginDialog = document.querySelector('#loginDialog');
+var loginBtn = document.querySelector('#loginBtn');
+var closeLoginDialogBtn = document.querySelector("#closeLoginDialogBtn");
 loginBtn.addEventListener("click", function (e) {
   e.preventDefault();
   loginDialog.parentElement.classList.toggle('hidden');
@@ -20,88 +23,75 @@ closeLoginDialogBtn.addEventListener("click", function (e) {
   e.preventDefault();
   loginDialog.parentElement.classList.toggle('hidden');
   loginDialog.close();
-});
+}); // 付款進度條
 
-// 付款進度條
-$('#goPay').on('click', (e) => {
+$('#goPay').on('click', function (e) {
   $('#timeline').toggleClass('before:hidden');
   $('#timeline').toggleClass('before:block');
-
   $('#payStep1').toggleClass("before:content-['1']");
   $('#payStep1').toggleClass("before:content-checked");
-
   $('#payStep2').toggleClass('before:bg-dark-300');
   $('#payStep2').toggleClass('before:bg-green');
   $('#payStep2').toggleClass('before:border');
   $('#payStep2').toggleClass('text-green');
-
   $('#lessonSignUp').fadeOut();
   $('#payInfo').fadeIn();
-})
-
-$('#pay').on('click', (e) =>{
+});
+$('#pay').on('click', function (e) {
   $('#timeline').toggleClass('after:hidden');
   $('#timeline').toggleClass('after:block');
-
   $('#payStep2').toggleClass("before:content-['2']");
   $('#payStep2').toggleClass("before:content-checked");
   $('#payStep3').toggleClass("before:content-['3']");
   $('#payStep3').toggleClass("before:content-checked");
-
   $('#payStep3').toggleClass('before:bg-dark-300');
   $('#payStep3').toggleClass('before:bg-green');
   $('#payStep3').toggleClass('before:border');
   $('#payStep3').toggleClass('text-green');
-
   $('#payInfo').fadeOut();
   $('#paySuccess').toggleClass('md:flex');
-})
+}); //輪播
 
-
-//輪播
-let swiperCourse = new Swiper('#swiperCourse', {
+var swiperCourse = new Swiper('#swiperCourse', {
   autoplay: {
     delay: 500,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   speed: 1500,
   slidesPerView: 'auto',
-  loop: true,
-})
-
-
-let swiperSuggest = new Swiper("#swiperSuggest", {
+  loop: true
+});
+var swiperSuggest = new Swiper("#swiperSuggest", {
   autoplay: {
     delay: 3000,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   slidesPerView: 1,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable: true
   },
   navigation: {
     nextEl: ".button-next",
-    prevEl: ".button-prev",
-  },
+    prevEl: ".button-prev"
+  }
 });
-
-let swiperClass = new Swiper('#swiperClass', {
+var swiperClass = new Swiper('#swiperClass', {
   freeMode: true,
   mousewheel: {
-    forceToAxis: true,
+    forceToAxis: true
   },
   slidesPerView: 'auto',
   spaceBetween: 16,
-  observer: true,//修改swiper自己或子元素时，自动初始化swiper
-  observeParents: true,
-})
-
-const elem = document.querySelector('input[name="datepicker"]');
-const datepicker = new Datepicker(elem, {
+  observer: true,
+  //修改swiper自己或子元素时，自动初始化swiper
+  observeParents: true
+});
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
   language: 'zh-TW',
   orientation: 'bottom',
-  autohide: true,
+  autohide: true
 });
-
+//# sourceMappingURL=all.js.map
